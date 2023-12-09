9b2bdb068353b0ed5637c9962edd2de8117cc6cb
GabrielSilper <gabrielsilper.main@gmail.com>
2023-12-09 19:33:45 -0400

    feat: Primeiro query com GraphQL, um Hello World

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 const { ApolloServer, gql } = require('apollo-server');
 
-const typeDefs = gql``;
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
 
-const resolvers = {};
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world',
+  },
+};
 
-const server = new ApolloServer({ typeDefs, resolvers })
+const server = new ApolloServer({ typeDefs, resolvers });
+
+async function initServer() {
+  const url = await server.listen({port: process.env.SERVER_PORT});
+  console.log(`Server graphql started at ${url.port}`);
+}
+
+initServer();
